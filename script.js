86af7aa05269a210aa5011b3cb07ad0776a6bc4a
edwin <Edwinslottje@gmail.com>
2024-11-05 20:43:48 +0100

    commit for 2.0

---
+++ script.js
@@ -0,0 +1,24 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll("header.hero nav a");
+
+    window.addEventListener("scroll", function () {
+        let currentSection = "";
+
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop - 500; //adjust offset top if bugging 
+            const sectionHeight = section.clientHeight;
+            if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
+                currentSection = section.getAttribute("id");
+            }
+        });
+
+        navLinks.forEach((link) => {
+            link.classList.remove("active");
+            if (link.getAttribute("href").substring(1) === currentSection) {
+                link.classList.add("active");
+            }
+        });
+    });
+});
